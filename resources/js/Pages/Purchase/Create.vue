e6bd17373f70bf7414db6bb9499d1bb34a8d31fc
Xiao Xue <xxue8761@conestogac.on.ca>
2025-02-24 16:03:01 -0500

    Initial commit

---
+++ resources/js/Pages/Purchase/Create.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useForm } from "@inertiajs/vue3";
+
+const form = useForm({
+    name: "",
+    amount: 0,
+    category: "",
+});
+</script>
+
+<template>
+    Store the record!
+    <form
+        @submit.prevent="form.post('/purchase')"
+        method="post"
+        class="flex flex-col gap-6"
+    >
+        <label>
+            name
+            <input type="text" v-model="form.name" />
+        </label>
+
+        <label>
+            amount
+            <input type="number" v-model="form.amount" />
+        </label>
+        <label>
+            category
+            <input type="text" v-model="form.category" />
+        </label>
+        <button type="submit">submit</button>
+    </form>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Pages/Purchase/Index.vue
@@ -0,0 +1,30 @@
+<script setup>
+    defineProps({
+        'purchases': Array,
+    })
+
+</script>
+
+<template>
+    <p class="text-red-200"> Is it gon be here?</p>
+    <table >
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Category</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="purchase in purchases">
+                <td>{{ purchase.name }}</td>
+                <td>{{ purchase.amount }}</td>
+                <td>{{ purchase.category }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<style scoped>
+
+</style>
