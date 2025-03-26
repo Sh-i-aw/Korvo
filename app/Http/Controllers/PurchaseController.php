9d356e8be8a89ae9da4a41aae2359ea2522375b6
Xiao Xue <xxue8761@conestogac.on.ca>
2025-03-26 17:41:54 -0400

    Category table, factory, seeder Loading Purchases with Category when indexed

--- app/Http/Controllers/PurchaseController.php
+++ app/Http/Controllers/PurchaseController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePurchaseRequest;
 use App\Models\Category;
 use App\Models\Purchase;
-use Illuminate\Http\Request;
+use Illuminate\Support\Number;
 use Inertia\Inertia;
 
 class PurchaseController extends Controller
 {
     public function index() {
-        $purchases = Purchase::all();
+        $purchases = Purchase::query()->with('category')->get();
+        collect($purchases)->each(function ($purchase) {
+            $purchase->amount = Number::currency($purchase->amount / 100);
+        });
         return Inertia::render('Purchase/Index', ['purchases' => $purchases]);
     }
 
@@ -21,7 +24,6 @@ class PurchaseController extends Controller
     }
     public function store (StorePurchaseRequest $request) {
         $data = $request->validated();
-
         Purchase::query()->create($data);
 
         return redirect()->route('purchase.index');
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        Category::factory()->count(10)->create();
+        $this->call([
+            CategorySeeder::class,
+        ]);
     }
 }
